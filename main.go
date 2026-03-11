ead19ac56e3b1a7d16bad8b4063dfbc4d949c504
KervyN <bb@kervyn.de>
2026-03-11 18:16:19 +0100

    move repl logic from main.go to repl.go

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())
-		fmt.Println("Your command was:", input[0])
-	}
+	startRepl()
 }
