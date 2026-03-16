e373e30d29fce7fd487ca50c91bb297d036b791f
KervyN <bb@kervyn.de>
2026-03-16 09:49:03 +0100

    transfer push to other workstation

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -6,11 +6,30 @@ import (
 )
 
 type Cache struct {
-	val map[string]cacheEntry
-	mu  sync.Mutex
+	entries map[string]cacheEntry
+	mu      sync.Mutex
 }
 
 type cacheEntry struct {
 	createdAt time.Time
 	val       []byte
+}
+
+func NewCache(interval int) (*Cache, error) {
+	cache := Cache{}
+
+	reapLoop(cache, interval)
+	return cache, nil
+}
+
+func Add(cache *Cache) error {
+	return nil
+}
+
+func Get(cache *Cache) ([]byte, bool) {
+	return nil, false
+}
+
+func reapLoop(cache *Cache, interval time.Duration) {
+
 }
